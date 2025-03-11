63c4934700acd2983e93c274cce28f956e8d918c
Headliner770 <paul248022@gmail.com>
2025-03-11 14:22:37 +0300

    CustomEvent

---
+++ custom-events/animateSection.js
@@ -0,0 +1,12 @@
+// Генерация событий. на примере сайта ламкова (лоадер при загрузки)
+const sectionElements = document.querySelectorAll("section");
+
+const animateSections = () => {
+  sectionElements.forEach((sectionElement) => {
+    sectionElement.classList.add("is-visible"); // проитерируемся по коллекции элем sectionEl. На каждой итерации у нас есть доступ к дом-элем конкретной секции sectionEl. добавляем этому элем кл is-visible
+  });
+};
+
+document.addEventListener("preloaderClose", () => {
+  animateSections();
+});
