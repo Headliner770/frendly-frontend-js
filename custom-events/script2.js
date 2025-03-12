16749f068db7c9318a66f2a924242c5b24f2362f
Headliner770 <paul248022@gmail.com>
2025-03-12 17:57:29 +0300

    Drag and Drop

--- custom-events/script2.js
+++ custom-events/script2.js
@@ -1007,3 +1007,105 @@
 // gotpointercapture
 // lostpointercapture
 // Преимущества соб указателя над событиями мыши. У соб указателя есть доп св-ва, которых нет у устаревших соб мыши.
+
+//  Мультитач
+// const addCircleElement = (x, y, isRed, label) => {
+//   const circleElementMarkup = `
+//   <div
+//   class="circle ${isRed ? "red" : ""}"
+//   style="
+//   top: ${y}px;
+//   left: ${x}px;
+//   "
+//   >
+//   <span class='circle__label'>${label}</span>
+//   </div>
+//   `;
+//   document.body.insertAdjacentHTML("beforeend", circleElementMarkup);
+// };
+// document.addEventListener("pointerdown", (event) => {
+//   console.log(event);
+//   const { x, y, isPrimary, pointerId } = event;
+//   addCircleElement(x, y, isPrimary, pointerId);
+// });
+// с помощью мыши будут только красные круги.
+// но если открыть данную страницу на телефоне, то после тапа будет так же красный кружок, но уже с уникальным поинерID. При юзании мультитач, первый кружок будет красным, все остальные касания сгенерят синие круги, так же с уникальным поинерID.
+
+// Drag and Drop
+// class DragAndDrop {
+//   selectors = {
+//     root: "[data-js-dnd]",
+//   };
+//   stateClasses = {
+//     isDragging: "is-dragging",
+//   };
+//   initialState = {
+//     offsetX: null,
+//     offsetY: null,
+//     isDragging: false,
+//     currentDraggingElement: null,
+//   };
+//   constructor() {
+//     this.state = { ...this.initialState }; // развернем в начальное состояние
+//     this.bindEvents();
+//   }
+
+//   resetState() {
+//     this.state = { ...this.initialState };
+//   }
+
+//   onPointerDown(event) {
+//     const { target, x, y } = event;
+//     const isDraggable = target.matches(this.selectors.root);
+//     if (!isDraggable) {
+//       return;
+//     }
+
+//     target.classList.add(this.stateClasses.isDragging);
+
+//     const { left, top } = target.getBoundingClientRect(); // получаем координаты элементов
+
+//     this.state = {
+//       offsetX: x - left,
+//       offsetY: y - top,
+//       isDragging: true,
+//       currentDraggingElement: target,
+//     };
+//   }
+
+//   onPointerMove(event) {
+//     if (!this.state.isDragging) {
+//       return;
+//     }
+
+//     const x = event.pageX - this.state.offsetX;
+//     const y = event.pageY - this.state.offsetY;
+
+//     this.state.currentDraggingElement.style.left = `${x}px`;
+//     this.state.currentDraggingElement.style.top = `${y}px`;
+//   }
+
+//   onPointerUp() {
+//     if (!this.state.isDragging) {
+//       return;
+//     }
+
+//     this.state.currentDraggingElement.classList.remove(
+//       this.stateClasses.isDragging
+//     );
+//     this.resetState();
+//   }
+
+//   bindEvents() {
+//     document.addEventListener("pointerdown", (event) =>
+//       this.onPointerDown(event)
+//     );
+//     document.addEventListener("pointermove", (event) =>
+//       this.onPointerMove(event)
+//     );
+//     document.addEventListener("pointerup", () => this.onPointerUp());
+//   }
+// }
+
+// new DragAndDrop();
+// Еду можно перетаскивать котику и оставлять. Логика с перетаскиванием пищи у меня не получилась (((( хз чё делать.
