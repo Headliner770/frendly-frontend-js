49433c3da57119bfac413b88400ff21fa8f839b7
Headliner770 <paul248022@gmail.com>
2025-08-06 18:23:24 +0300

    go

--- custom-events/tasks.js
+++ custom-events/tasks.js
@@ -1,32 +1,54 @@
-//     1     //
+//      1      //
 // const createCounter = (n) => {
-//   let count = n;
+//   let counter = n;
 
 //   return () => {
-//     return count++;
+//     return counter++;
 //   };
 // };
 
 // const counter = createCounter(10);
-// console.log(counter());
-// console.log(counter());
-// console.log(counter());
+// console.log(counter()); // 10
+// console.log(counter()); // 11
+// console.log(counter()); // 12
 
-//      2       //
-const createCounter = function (init) {
-  let counter = init;
+//      2      //
+// const createCounter = function (init) {
+//   let counter = init;
 
-  return {
-    increment: () => ++counter,
-    decrement: () => --counter,
-    reset: () => {
-      counter = init;
-      return counter;
-    },
-  };
-};
+//   return {
+//     increment: () => 
+//       ++counter,
+//     decrement: () => 
+//       --counter,
+//     reset: () => {
+//       counter = init;
+//       return counter;
+//     },
+//   };
+// };
 
-const counter = createCounter(5);
-console.log(counter.increment()); // 6
-console.log(counter.reset()); // 5
-console.log(counter.decrement()); // 4
+// const counter = createCounter(5);
+// console.log(counter.increment()); // 6
+// console.log(counter.reset()); // 5
+// console.log(counter.decrement()); // 4
+
+// const colorConfig = {
+//   red: true,
+//   blue: false,
+//   green: true,
+//   black: true,
+//   yellow: false,
+// };
+
+// const colors = ["pink", "red", "blue"];
+
+// console.log(colorConfig[colors[1]]);
+
+let a = 2;
+let b = a;
+
+a = 6;
+
+console.log(a)
+console.log(b)
